4f32bedbc8db78360aff23d1327c90eb2adcdcfe
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-10-23 17:30:14 +0000

    API

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,39 +5,14 @@ import requests
 import sys
 
 if __name__ == "__main__":
-
-    if len(sys.argv) != 2:
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    user_response = requests.get(url + "users/{}".format(employee_id))
-    if user_response.status_code != 200:
-        print("Employee not found.")
-        sys.exit(1)
-
-    user_data = user_response.json()
-    username = user_data.get("username")
-
-    todo_response = requests.get(url + "todos", params={"userId": employee_id})
-    todo_data = todo_response.json()
-
-    with open("{}.csv".format(employee_id), "w", newline="") as csvfile:
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-
-        writer.writerow([
-            "USER_ID",
-            "USERNAME",
-            "TASK_COMPLETED_STATUS",
-            "TASK_TITLE"
-        ])
-
-        for task in todo_data:
-            writer.writerow([
-                employee_id,
-                username,
-                task["completed"],
-                task["title"]
-            ])
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
