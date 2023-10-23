bebf234f7f9b7b9672aa88bc299e6ae7598390e8
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-10-23 17:38:17 +0000

    API

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,37 +1,19 @@
+#!/usr/bin/python3
+"""export data in the JSON format"""
 import json
 import requests
 import sys
 
 if __name__ == "__main__":
-
-    if len(sys.argv) != 2:
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    user_response = requests.get(url + "users/{}".format(employee_id))
-    if user_response.status_code != 200:
-        print("Employee not found.")
-        sys.exit(1)
-
-    user_data = user_response.json()
-    username = user_data.get("username")
-
-    todo_response = requests.get(url + "todos", params={"userId": employee_id})
-    todo_data = todo_response.json()
-
-    user_dict = {
-        employee_id: [
-            {
-                "task": task["title"],
-                "completed": task["completed"],
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
                 "username": username
-            }
-            for task in todo_data
-        ]
-    }
-
-    with open("{}.json".format(employee_id), "w") as jsonfile:
-        json.dump(user_dict, jsonfile)
+            } for t in todos]}, jsonfile)
