4953c6bab6cf5b1a12cb3311e649a534f3e51257
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-11-07 21:02:21 +0100

    api_advenced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,14 +9,13 @@ def number_of_subscribers(subreddit):
     if not subreddit or not isinstance(subreddit, str):
         return 0
 
+    user_agent = {'User-agent': 'Your User Agent Here'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    response = requests.get(url)
+    response = requests.get(url, headers=user_agent)
 
     if response.status_code == 200:
         data = response.json().get('data')
-        if data:
-            return data.get('subscribers')
-        else:
-            return 0
+        return data.get('subscribers') if data else 0
     else:
         return 0
+
