52f1d6e8d140de26c07e92cb72ea30ae0876896f
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-11-07 21:48:36 +0100

    api advenced

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import requests
+
+
+def top_ten(subreddit):
+    if not subreddit or not isinstance(subreddit, str):
+        print("None")
+        return
+
+    user_agent = {'User-agent': 'soufiane'}
+    params = {'limit': 10}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+
+    response = requests.get(url, headers=user_agent, params=params)
+
+    if response.status_code == 200:
+        data = response.json().get('data', {}).get('children', [])
+
+        for item in data:
+            title = item.get('data', {}).get('title', '')
+            print(title)
+    else:
+        print("None")
